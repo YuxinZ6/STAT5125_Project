c947ff2df91befca67b3b45c351f1b4ae4e76634
Yuxin Zhang <yuxin.6.zhang@gmail.com>
2023-04-10 00:56:45 -0400

    finished scatter plot. Stuck on the XGboost model, will need to consult.

--- Model_and_graphics_Yuxin.R
+++ Model_and_graphics_Yuxin.R
@@ -3,6 +3,12 @@
 # Random Forest, XG-Boost
 # visualizations are as the following: 
 # scatter plot
+unregister_dopar <- function() {
+  env <- foreach:::.foreachGlobals
+  rm(list=ls(name=env), pos=env)
+}
+
+unregister_dopar()
 
 ###################################################################
 # Random Forest
@@ -15,6 +21,8 @@ library(parallel)
 library(doParallel)
 library(tidymodels)
 library(yardstick)
+library(ggplot2)
+library(xgboost)
 
 ####Result of the following code, up until line 62 has been saved under###
 ####file = "RandomForest_Prediction.rds". Run line 65 to obtain result.###
@@ -75,3 +83,88 @@ Best_result2 <- rf_tuning %>%
 Best_result <- Best_result1 %>% rbind(Best_result2)
 Best_result
 
+###################################################################
+# XG-Boost
+
+# XG-boost requires train object and test object column names and column
+# order to be exactly the same. 
+train_sorted <- train[, order(names(train))]
+test_sorted <- test[, order(names(test))]
+
+# set the seed
+set.seed(123457)
+# define parsnip, set random forest with mtry, trees, and min_n hyperparameters
+# as tuning. To be tuned later.
+parsnip_boost <- boost_tree(tree_depth = tune("tree_depth"), 
+                            learn_rate = tune("learn_rate"), 
+                            min_n = tune("min_n")) %>%
+  set_engine('xgboost') %>%
+  set_mode('regression')
+
+# define workflow
+workflow_boost <- workflow() %>% 
+  add_model(parsnip_boost) %>%
+  add_formula(Disaster_Frequency ~ .)
+
+# define hyperparameter tuning grid to be experimented
+hyperparam_tune_grid_boost <- crossing(min_n = seq(10, 50, by = 20),
+                                 tree_depth = c(5, 10, 15),
+                                 learn_rate = c(0.01, 0.05, 0.1))
+# define cross validation
+boost_cv <- train_sorted %>% vfold_cv(v = 10, times = 2)
+# define metrics to be used
+boost_metrics <- metric_set(rmse, mae, rsq_trad)
+
+# assigning resources
+cl <- makePSOCKcluster(6)
+registerDoParallel(cl)
+time1 <- Sys.time() #save current system time
+
+#run the 10 fold cross validation for each combination of hyperparameters
+boost_tuning <- workflow_boost %>% 
+  tune_grid(resamples = boost_cv, 
+            grid = hyperparam_tune_grid_boost,
+            metrics = boost_metrics) %>%
+  collect_metrics()
+
+time2 <- Sys.time() #save current system time
+
+(diff <- time2 - time1) #obtain total run time
+
+stopCluster(cl)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+###################################################################
+# Visualizations: 
+scatterplot_df <- df_long %>% group_by(ISO3, Year) %>%
+  reframe(emission = sum(`Value`),
+          Disaster_Frequency = first(Disaster_Frequency)) %>%
+  glimpse()
+
+scatterplot_df$ISO3 <- as.factor(scatterplot_df$ISO3)
+
+scatterplot_df %>% ggplot(aes(x = log(emission), y = Disaster_Frequency)) + 
+  geom_point() + 
+  geom_point(data = scatterplot_df[scatterplot_df$Disaster_Frequency>10, ],
+             aes(x = log(emission), y = Disaster_Frequency, 
+                 color = ISO3)) +
+  facet_wrap(~Year) +
+  ylab("Disaster Count") + 
+  xlab("Log of Emission (Log of Metric Tons of CO2)") +
+  ggtitle("Emission versus Disaster Count for Each Country")
