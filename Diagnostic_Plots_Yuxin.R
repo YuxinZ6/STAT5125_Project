ae9fd43e9adf0e2e92312a4b7fd052d75769818f
YuxinZ6 <121071528+YuxinZ6@users.noreply.github.com>
2023-04-18 17:35:05 -0400

    adjusted the diagnostic plots

--- Diagnostic_Plots_Yuxin.R
+++ Diagnostic_Plots_Yuxin.R
@@ -5,28 +5,39 @@ library(caret)
 # Diagnostic Plots
 
 # setting up plot environment
-par(mfrow = c(2,2), mar = c(4,4,7,2) + 0.1)
+par(mfrow = c(2,2), mar = c(4,4,5,4))
+
 # Scatter plot of Actual versus predicted
 plot(train_sub$Disaster_Frequency, RF_train_predict$.pred,
      main = "Actual versus Predicted",
      ylab = "Prediction Result",
      xlab = "Actual Result")
-#qq plot
-qqPlot(RF_train_predict$.pred, main = "QQ-norm Plot")
+
+# calculate residuals
+resid <- train_sub$Disaster_Frequency - RF_train_predict$.pred
+
 # fitted versus residual plot
-train_sub$Disaster_Frequency - RF_train_predict$.pred
 fitted_resid_plot <- plot(RF_train_predict$.pred, resid,
                           ylab = "Residuals",
                           xlab = "Fitted Values",
                           main = "Fitted versus Residuals")
 abline(h = 0, col = 'blue', lty = 2)
-#calcualte standard residuals
+
+# calcualte standard residuals
 stand_resid <- (resid - mean(resid))/sd(resid)
+
 # fitted versus standard residual plot
 plot(RF_train_predict$.pred, stand_resid,
      ylab = "Standard Residuals",
      xlab = "Fitted Values",
      main = "Fitted versus Standard Residuals")
 abline(h = 0, col = 'blue', lty = 2)
+
+# qq plot
+qqPlot(stand_resid, main = "QQ-norm Plot",
+       id = FALSE, 
+       ylab = "Standard Residuals",
+       xlab = "Norm Quantiles")
+
 # setting a common title
 mtext("Diagnostic Plots", side = 3, line = -2, cex = 1.5, outer = TRUE)
